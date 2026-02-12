f1ddd94d562785105bc8d6dd31ba07f41b7f3877
Connor <connor.j.nguyen@outlook.com>
2026-02-12 15:11:23 -0800

    update submit.sh
    add names / uwid

--- submit.sh
+++ submit.sh
@@ -6,7 +6,7 @@ rm -rf submit submit.zip
 mkdir -p submit
 
 # submit team.txt
-printf "Victor Zhong,vzhong\nNoah Smith,nasmith\nSanta Claus,sclaus" > submit/team.txt
+printf "Connor Nguyen,cn05\nArjun Tiwari,atiw\nAnanya Aatreya,aatreya" > submit/team.txt
 
 # train model
 python src/myprogram.py train --work_dir work
